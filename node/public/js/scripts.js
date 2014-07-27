5e21aa154c0d44cc40a03910bd57c84c3798c38c
ask616 <areeb.khan616@gmail.com>
2014-07-27 08:07:26 -0700

    map reframe (almost finished)

--- node/public/js/scripts.js
+++ node/public/js/scripts.js
@@ -5,14 +5,13 @@ $(document).ready(function(){
 	    // an error occurred while attempting login
 	    console.log(error);
 	  } else if (user) {
-	     if( true ) {
+
 	      // save new user's profile into Firebase so we can
 	      // list users, use them in security rules, and show profiles
-	      myRef.child('users').child(user.uid).set({
-	        displayName: user.displayName,
+	      myRef.child('users').child(user.displayName).update({
 	        data:user.thirdPartyUserData
 	      });
-    	}
+
 	  } else {
 	    // user is logged out
 	  }
@@ -31,7 +30,7 @@ $(document).ready(function(){
 	$('#fb-login').click(function(){
 	auth.login('facebook', {
 	  rememberMe: true,
-	  scope: 'email,user_friends'
+	  scope: 'user_friends'
 	});
 });
 
