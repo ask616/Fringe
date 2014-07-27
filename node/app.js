e16f9b52e3c91d3121ad8e45214c738b4990ab73
ask616 <areeb.khan616@gmail.com>
2014-07-27 04:12:46 -0700

    integrated map

--- node/app.js
+++ node/app.js
@@ -92,6 +92,10 @@ app.get('/account', ensureAuthenticated, function(req, res){
   res.render('account', { user: req.user });
 });
 
+app.get('/map', function(req, res){
+  res.render('map.ejs', {});
+});
+
 app.get('/login', function(req, res){
   res.render('login', { user: req.user });
 });
